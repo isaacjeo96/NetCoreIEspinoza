cceda0980f1a3ce5e9eb71a51ea90accd5ae8e48
isaacjeo96 <isaacjeo96@outlook.com>
2022-03-16 23:34:53 -0600

    Agregar archivos de proyecto.

---
+++ DL/IEspinozaNETCoreContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DL
+{
+    public partial class IEspinozaNETCoreContext : DbContext
+    {
+        public IEspinozaNETCoreContext()
+        {
+        }
+
+        public IEspinozaNETCoreContext(DbContextOptions<IEspinozaNETCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Rol> Rols { get; set; } = null!;
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=WIN-PC-ISAAC; Database= IEspinozaNETCore; Trusted_Connection=True; Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Rol>(entity =>
+            {
+                entity.HasKey(e => e.IdRol)
+                    .HasName("PK__Rol__2A49584C0CC0ED71");
+
+                entity.ToTable("Rol");
+
+                entity.Property(e => e.IdRol).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuario__5B65BF979E8FB837");
+
+                entity.ToTable("Usuario");
+
+                entity.Property(e => e.ApellidoMaterno)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ApellidoPaterno)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(254)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdRolNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdRol)
+                    .HasConstraintName("FK__Usuario__IdRol__2C3393D0");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
